1e11168230dbc12a6a421cf39f50c5df5b8a18e4
sdwalker <sdwalker@wm.edu>
2023-09-21 19:37:19 -0400

    add calculate compatibility function

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from math import abs
 
-def calculate_compatibility(name1, name2):
-    pass
-    #TODO Add compatibility calculator
+def calculate_compatibility(name1: str, name2: str):
+    names = [name1, name2]
+    seed = "".join(names)
+    gen = random.Random(seed)
+    compatibility = (gen.random()) * 100 + 1
+    return int(compatibility)
 
 if __name__ == "__main__":
     print("Hello, world") 
