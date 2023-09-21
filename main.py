0cb1d126c50898c80951834d728149d19b00af5b
sdwalker <sdwalker@wm.edu>
2023-09-21 19:22:30 -0400

    Initial commit

--- main.py
+++ main.py
@@ -5,5 +5,4 @@ def calculate_compatibility(name1, name2):
     #TODO Add compatibility calculator
 
 if __name__ == "__main__":
-    pass
-    #TODO Add your main section code here
+    print("Hello, world") 
